3ff83f43a7fb136d54f22f073efcfb14dfcf8571
bdclauser <pzljug@hotmail.com>
2020-11-18 20:17:34 -0500

    switching tasks

--- bot.py
+++ bot.py
@@ -1,8 +1,30 @@
-import requests
-import time
+#!/usr/bin/env python3
+from selenium.common.exceptions import *
+from selenium.webdriver.support.ui import Select
+from selenmium.webdriver.common.action_chains import *
 
-status = "sold"
+import sys, __future__, os, time
 
+# grabs a config file and stores it as a tuple
+def getConfig();
+url = ""
+size = ""
+name = ""
+email = ""
+tel = ""
+address = ""
+zipCode = ""
+city = ""
+state = ""
+country = ""
+cardType = ""
+number = ""
+expMonth = ""
+expYear = ""
+CVV = ""
 
-r = requests.get("webpage")
-r.status_code
+conf = open('config.txt')
+line = conf.readline()
+while line != "":
+    words = line.split()
+    if len(words)
